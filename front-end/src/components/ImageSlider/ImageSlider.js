2c387ba72943be14d1605893c8934eb56fc7cf68
ktiya23 <kashishagrahari23@gmail.com>
2024-10-15 21:13:36 +0530

    big basket clone

--- front-end/src/components/ImageSlider/ImageSlider.js
+++ front-end/src/components/ImageSlider/ImageSlider.js
@@ -1,41 +1,78 @@
-import { Box  , HStack, Image} from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react'
+import { Box, HStack, Image, Button } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 
-const images =[
-    "https://www.bigbasket.com/media/uploads/banner_images/IBBN092113357-26109.jpg?tr=w-1920,q=80",
-    "https://www.bigbasket.com/media/uploads/banner_images/IBBN092113357-26112.jpg?tr=w-1920,q=80",
-    "https://www.bigbasket.com/media/uploads/banner_images/IBBN092113357-26108.jpg?tr=w-1920,q=80"
-]
+const images = [
+  "https://www.bigbasket.com/media/uploads/banner_images/IBBN092113357-26109.jpg?tr=w-1920,q=80",
+  "https://www.bigbasket.com/media/uploads/banner_images/IBBN092113357-26112.jpg?tr=w-1920,q=80",
+  "https://www.bigbasket.com/media/uploads/banner_images/IBBN092113357-26108.jpg?tr=w-1920,q=80"
+];
+
+const offerLabels = [
+  "Offers on Domex",
+  "Offers on Dove",
+  "Offers on Surf Excel"
+];
 
 const ImageSlider = () => {
+  const [currIndex, setCurrIndex] = useState(0);
 
-    const [currIndex , setCurrIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrIndex((prev) =>
+        prev === images.length - 1 ? 0 : prev + 1
+      );
+    }, 3000);
 
-    useEffect(()=>{
-        const interval = setInterval(()=>{
-            setCurrIndex((prev)=>
-                prev === images.length -1 ? 0 : prev +1
-            )
-        },2000)
+    return () => clearInterval(interval);
+  }, []);
 
-    return ()=> clearInterval(interval)
-    },[])
+  const handleSlide = (i) => {
+    setCurrIndex(i);
+  };
 
-    const handleSlide =(i)=>{
-        setCurrIndex(i);
-    }
   return (
-    <Box>
-        <Image src={images[currIndex]} boxSize="800px" objectFit="cover" transition="all 0.5s ease-in-out" />
-
-        <HStack>
-        {images.map((_, index) => (
-          
+    <Box width="100%" maxW="1200px" mx="auto" position="relative" overflow="hidden">
+      {/* Image display */}
+      <Box
+        display="flex"
+        width="300%"
+        transform={`translateX(-${currIndex * 100}%)`}
+        transition="transform 0.5s ease-in-out"
+      >
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            width="100%"
+            objectFit="cover"
+            borderRadius="md"
+          />
         ))}
-        </HStack>
+      </Box>
+
+      
+
+      <HStack
+        position="absolute"
+        bottom="5px"
+        left="50%"
+        transform="translateX(-50%)"
+        spacing={4}
+        zIndex="1"
+      >
+        {offerLabels.map((label, index) => (
+          <Button
+            key={index}
+            size="sm"
+            colorScheme={currIndex === index ? "green" : "gray"}
+            onClick={() => handleSlide(index)}
+          >
+            {label}
+          </Button>
+        ))}
+      </HStack>
     </Box>
+  );
+};
 
-  )
-}
-
-export default ImageSlider
+export default ImageSlider;
